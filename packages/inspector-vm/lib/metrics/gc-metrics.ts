43891d92de6cad09530c6b542517e4cdeff7ca20
rstiller <rstiller@users.noreply.github.com>
2017-05-28 20:17:22 +0200

    added editorconfig

--- packages/inspector-vm/lib/metrics/gc-metrics.ts
+++ packages/inspector-vm/lib/metrics/gc-metrics.ts
@@ -1,9 +1,43 @@
-
 import "source-map-support/register";
 
-import { BaseMetric, Clock, DefaultReservoir, Metric, MetricSet, NANOSECOND, SimpleGauge, Timer } from "inspector-metrics";
+import {
+    BaseMetric,
+    Clock,
+    DefaultReservoir,
+    Metric,
+    MetricSet,
+    NANOSECOND,
+    SimpleGauge,
+    Timer,
+} from "inspector-metrics";
 
-const gc = require("gc-stats");
+const gc = require("gc-heap-stats");
+
+export interface MemoryStats {
+    type: number;
+    took: number;
+    before: MemoryMetadataSnapshot;
+    after: MemoryMetadataSnapshot;
+    diff: MemoryMetadataSnapshot;
+}
+
+export interface MemoryMetadataSnapshot {
+    total_heap_size: number;
+    total_heap_size_executable: number;
+    total_physical_size: number;
+    total_available_size: number;
+    used_heap_size: number;
+    heap_size_limit: number;
+    spaces: Space[];
+}
+
+export interface Space {
+    space_name: string;
+    space_size: number;
+    space_used_size: number;
+    space_available_size: number;
+    physical_space_size: number;
+}
 
 export class GCMetrics extends BaseMetric implements MetricSet {
 
@@ -14,6 +48,8 @@ export class GCMetrics extends BaseMetric implements MetricSet {
     private phantomCallbackProcessingRuns: Timer;
     private allRuns: Timer;
     private totalHeapSize: SimpleGauge = new SimpleGauge();
+    private totalAvailableSize: SimpleGauge = new SimpleGauge();
+    private totalPhysicalSize: SimpleGauge = new SimpleGauge();
     private totalHeapExecutableSize: SimpleGauge = new SimpleGauge();
     private usedHeapSize: SimpleGauge = new SimpleGauge();
     private heapSizeLimit: SimpleGauge = new SimpleGauge();
@@ -42,32 +78,36 @@ export class GCMetrics extends BaseMetric implements MetricSet {
         this.metrics.set("phantomCallbackProcessingRuns", this.phantomCallbackProcessingRuns);
         this.metrics.set("allGCRuns", this.allRuns);
         this.metrics.set("totalHeapSize", this.totalHeapSize);
+        this.metrics.set("totalPhysicalSize", this.totalPhysicalSize);
+        this.metrics.set("totalAvailableSize", this.totalAvailableSize);
         this.metrics.set("totalHeapExecutableSize", this.totalHeapExecutableSize);
         this.metrics.set("usedHeapSize", this.usedHeapSize);
         this.metrics.set("heapSizeLimit", this.heapSizeLimit);
 
-        gc.on("stats", (stats: any) => {
-            switch (stats.gctype) {
+        gc().on("stats", (stats: MemoryStats) => {
+            switch (stats.type) {
                 case 1:
-                    this.minorRuns.addDuration(stats.pause, NANOSECOND);
+                    this.minorRuns.addDuration(stats.took * 1000000, NANOSECOND);
                     break;
                 case 2:
-                    this.majorRuns.addDuration(stats.pause, NANOSECOND);
+                    this.majorRuns.addDuration(stats.took * 1000000, NANOSECOND);
                     break;
                 case 4:
-                    this.incrementalMarkingRuns.addDuration(stats.pause, NANOSECOND);
+                    this.incrementalMarkingRuns.addDuration(stats.took * 1000000, NANOSECOND);
                     break;
                 case 8:
-                    this.phantomCallbackProcessingRuns.addDuration(stats.pause, NANOSECOND);
+                    this.phantomCallbackProcessingRuns.addDuration(stats.took * 1000000, NANOSECOND);
                     break;
                 default:
-                    this.allRuns.addDuration(stats.pause, NANOSECOND);
+                    this.allRuns.addDuration(stats.took * 1000000, NANOSECOND);
                     break;
             }
-            this.totalHeapSize.setValue(stats.after.totalHeapSize);
-            this.totalHeapExecutableSize.setValue(stats.after.totalHeapExecutableSize);
-            this.usedHeapSize.setValue(stats.after.usedHeapSize);
-            this.heapSizeLimit.setValue(stats.after.heapSizeLimit);
+            this.totalHeapSize.setValue(stats.after.total_heap_size);
+            this.totalPhysicalSize.setValue(stats.after.total_physical_size);
+            this.totalAvailableSize.setValue(stats.after.total_available_size);
+            this.totalHeapExecutableSize.setValue(stats.after.total_heap_size_executable);
+            this.usedHeapSize.setValue(stats.after.used_heap_size);
+            this.heapSizeLimit.setValue(stats.after.heap_size_limit);
         });
 
         this.setGroup("gc");
--- packages/inspector-vm/test/metrics/gc-metrics-test.ts
+++ packages/inspector-vm/test/metrics/gc-metrics-test.ts
@@ -1,16 +1,30 @@
-
+// tslint:disable:no-unused-expression
+import * as crypto from "crypto";
 import "source-map-support/register";
 
-// import * as chai from "chai";
-import { suite, test } from "mocha-typescript";
+import * as chai from "chai";
+import { StdClock, Timer } from "inspector-metrics";
+import { suite, test, timeout } from "mocha-typescript";
+import { GCMetrics } from "../../lib/metrics/gc-metrics";
 
-// const expect = chai.expect;
+const expect = chai.expect;
 
 @suite
-export class GCMetricsReporterTest {
+export class GCMetricsTest {
 
     @test
+    @timeout(60000)
     public checkNothing(): void {
+        const metric: GCMetrics = new GCMetrics(new StdClock());
+
+        expect(metric).to.exist;
+
+        const majorRuns: Timer = metric.getMetrics().get("majorGCRuns") as Timer;
+        // tslint:disable-next-line:no-console
+        console.log();
+        while (majorRuns.getCount() <= 0) {
+            crypto.randomBytes(1024);
+        }
     }
 
 }
