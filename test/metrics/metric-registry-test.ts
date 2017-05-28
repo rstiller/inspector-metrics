9e8c18714066e0198322723337626bdbdf129bd1
rstiller <rstiller@users.noreply.github.com>
2017-05-28 22:03:10 +0200

    fixed tests for v1.3

--- test/metrics/metric-registry-test.ts
+++ test/metrics/metric-registry-test.ts
@@ -46,37 +46,37 @@ export class MetricRegistryTest {
         const registry: MetricRegistry = new MetricRegistry();
 
         expect(registry.getCounters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
+        expect(registry.getMetrics()).to.have.length(0);
 
         const counter = registry.newCounter("counter1");
         expect(counter).to.be.not.null;
         expect(counter).to.be.instanceof(Counter);
 
         expect(registry.getCounters()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getCounter("counter1")).to.be.equal(counter);
         expect(registry.getMetric("counter1")).to.be.equal(counter);
 
         registry.removeCounter("counter1");
 
         expect(registry.getCounters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getCounter("counter1")).to.be.undefined;
-        expect(registry.getMetric("counter1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getCounter("counter1")).to.not.exist;
+        expect(registry.getMetric("counter1")).to.not.exist;
 
         registry.register("counter1", counter);
 
         expect(registry.getCounters()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getCounter("counter1")).to.be.equal(counter);
         expect(registry.getMetric("counter1")).to.be.equal(counter);
 
         registry.removeMetric("counter1");
 
         expect(registry.getCounters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getCounter("counter1")).to.be.undefined;
-        expect(registry.getMetric("counter1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getCounter("counter1")).to.not.exist;
+        expect(registry.getMetric("counter1")).to.not.exist;
     }
 
     @test
@@ -84,37 +84,37 @@ export class MetricRegistryTest {
         const registry: MetricRegistry = new MetricRegistry();
 
         expect(registry.getHistograms()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
+        expect(registry.getMetrics()).to.have.length(0);
 
         const histogram = registry.newHistogram("histogram1");
         expect(histogram).to.be.not.null;
         expect(histogram).to.be.instanceof(Histogram);
 
         expect(registry.getHistograms()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getHistogram("histogram1")).to.be.equal(histogram);
         expect(registry.getMetric("histogram1")).to.be.equal(histogram);
 
         registry.removeHistogram("histogram1");
 
         expect(registry.getHistograms()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getHistogram("histogram1")).to.be.undefined;
-        expect(registry.getMetric("histogram1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getHistogram("histogram1")).to.not.exist;
+        expect(registry.getMetric("histogram1")).to.not.exist;
 
         registry.register("histogram1", histogram);
 
         expect(registry.getHistograms()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getHistogram("histogram1")).to.be.equal(histogram);
         expect(registry.getMetric("histogram1")).to.be.equal(histogram);
 
         registry.removeMetric("histogram1");
 
         expect(registry.getHistograms()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getHistogram("histogram1")).to.be.undefined;
-        expect(registry.getMetric("histogram1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getHistogram("histogram1")).to.not.exist;
+        expect(registry.getMetric("histogram1")).to.not.exist;
     }
 
     @test
@@ -122,37 +122,37 @@ export class MetricRegistryTest {
         const registry: MetricRegistry = new MetricRegistry();
 
         expect(registry.getMeters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
+        expect(registry.getMetrics()).to.have.length(0);
 
         const meter = registry.newMeter("meter1");
         expect(meter).to.be.not.null;
         expect(meter).to.be.instanceof(Meter);
 
         expect(registry.getMeters()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getMeter("meter1")).to.be.equal(meter);
         expect(registry.getMetric("meter1")).to.be.equal(meter);
 
         registry.removeMeter("meter1");
 
         expect(registry.getMeters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getMeter("meter1")).to.be.undefined;
-        expect(registry.getMetric("meter1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getMeter("meter1")).to.not.exist;
+        expect(registry.getMetric("meter1")).to.not.exist;
 
         registry.register("meter1", meter);
 
         expect(registry.getMeters()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getMeter("meter1")).to.be.equal(meter);
         expect(registry.getMetric("meter1")).to.be.equal(meter);
 
         registry.removeMetric("meter1");
 
         expect(registry.getMeters()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getMeter("meter1")).to.be.undefined;
-        expect(registry.getMetric("meter1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getMeter("meter1")).to.not.exist;
+        expect(registry.getMetric("meter1")).to.not.exist;
     }
 
     @test
@@ -166,30 +166,30 @@ export class MetricRegistryTest {
         expect(timer).to.be.instanceof(Timer);
 
         expect(registry.getTimers()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getTimer("timer1")).to.be.equal(timer);
         expect(registry.getMetric("timer1")).to.be.equal(timer);
 
         registry.removeTimer("timer1");
 
         expect(registry.getTimers()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getTimer("timer1")).to.be.undefined;
-        expect(registry.getMetric("timer1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getTimer("timer1")).to.not.exist;
+        expect(registry.getMetric("timer1")).to.not.exist;
 
         registry.register("timer1", timer);
 
         expect(registry.getTimers()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getTimer("timer1")).to.be.equal(timer);
         expect(registry.getMetric("timer1")).to.be.equal(timer);
 
         registry.removeMetric("timer1");
 
         expect(registry.getTimers()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getTimer("timer1")).to.be.undefined;
-        expect(registry.getMetric("timer1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getTimer("timer1")).to.not.exist;
+        expect(registry.getMetric("timer1")).to.not.exist;
     }
 
     @test
@@ -197,12 +197,13 @@ export class MetricRegistryTest {
         const registry: MetricRegistry = new MetricRegistry();
 
         expect(registry.getCounters()).to.satisfy(mapSize(0));
-
         const counter = registry.newCounter("counter1");
+        expect(registry.getCounters()).to.satisfy(mapSize(1));
+
         registry.register("set1", registry);
 
         expect(registry.getCounters()).to.satisfy(mapSize(2));
-        expect(registry.getMetrics()).to.satisfy(mapSize(2));
+        expect(registry.getMetrics()).to.have.length(2);
         expect(registry.getCounter("counter1")).to.be.equal(counter);
         expect(registry.getCounter("set1.counter1")).to.be.equal(counter);
     }
@@ -218,7 +219,7 @@ export class MetricRegistryTest {
         registry.register("set1", registry);
 
         expect(registry.getCounters()).to.satisfy(mapSize(2));
-        expect(registry.getMetrics()).to.satisfy(mapSize(2));
+        expect(registry.getMetrics()).to.have.length(2);
         expect(registry.getCounter("counter1")).to.be.equal(counter);
         expect(registry.getCounter("constant-name")).to.be.equal(counter);
     }
@@ -228,35 +229,34 @@ export class MetricRegistryTest {
         const registry: MetricRegistry = new MetricRegistry();
 
         expect(registry.getGauges()).to.satisfy(mapSize(0));
-
         const gauge = new SimpleGauge();
         registry.register("gauge1", gauge);
 
         expect(registry.getGauges()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getGauge("gauge1")).to.be.equal(gauge);
         expect(registry.getMetric("gauge1")).to.be.equal(gauge);
 
         registry.removeGauge("gauge1");
 
         expect(registry.getGauges()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getGauge("gauge1")).to.be.undefined;
-        expect(registry.getMetric("gauge1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getGauge("gauge1")).to.not.exist;
+        expect(registry.getMetric("gauge1")).to.not.exist;
 
         registry.register("gauge1", gauge);
 
         expect(registry.getGauges()).to.satisfy(mapSize(1));
-        expect(registry.getMetrics()).to.satisfy(mapSize(1));
+        expect(registry.getMetrics()).to.have.length(1);
         expect(registry.getGauge("gauge1")).to.be.equal(gauge);
         expect(registry.getMetric("gauge1")).to.be.equal(gauge);
 
         registry.removeMetric("gauge1");
 
         expect(registry.getGauges()).to.satisfy(mapSize(0));
-        expect(registry.getMetrics()).to.satisfy(mapSize(0));
-        expect(registry.getGauge("gauge1")).to.be.undefined;
-        expect(registry.getMetric("gauge1")).to.be.undefined;
+        expect(registry.getMetrics()).to.have.length(0);
+        expect(registry.getGauge("gauge1")).to.not.exist;
+        expect(registry.getMetric("gauge1")).to.not.exist;
     }
 
     @test
