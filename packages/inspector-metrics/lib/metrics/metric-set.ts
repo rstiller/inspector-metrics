004152ab7a383a9ecf181c93a9ce0db6bdd49616
rstiller <rstiller@users.noreply.github.com>
2017-05-28 22:49:31 +0200

    added compatibility methods

--- packages/inspector-metrics/lib/metrics/metric-set.ts
+++ packages/inspector-metrics/lib/metrics/metric-set.ts
@@ -3,5 +3,9 @@ import "source-map-support/register";
 import { Metric } from "./metric";
 
 export interface MetricSet extends Metric {
-    getMetrics(): Metric[];
+    /**
+     * @deprecated since version 1.3 - use {@link getMetricList} instead
+     */
+    getMetrics(): Map<string, Metric>;
+    getMetricList(): Metric[];
 }
